c4097b5553603f0137d7c52d8ebbf1edb3123982
CharlesDeda <94919280+CharlesDeda@users.noreply.github.com>
2022-03-09 16:34:25 -0500

    Initial commit

---
+++ War-Challenge/War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Nick Deda on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
